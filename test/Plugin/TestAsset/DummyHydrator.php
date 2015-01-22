3bc8d1d4edbf59c7c9e2f8fa2feb3c5baa50d9f4
Stefano Torresi <webdev@stefanotorresi.it>
2015-01-22 21:15:28 +0100

    add failing test adding autoinvokable hydrator

---
+++ test/Plugin/TestAsset/DummyHydrator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Stefano Torresi (http://stefanotorresi.it)
+ * @license See the file LICENSE.txt for copying permission.
+ * ************************************************
+ */
+
+namespace ZFTest\Hal\Plugin\TestAsset;
+
+use Zend\Stdlib\Hydrator\ArraySerializable;
+
+class DummyHydrator extends ArraySerializable
+{
+
+}
