d160c571640b68a8ec4184bc181e3fe2c7dcccec
Nicolas Eeckeloo <neeckeloo@gmail.com>
2015-07-08 15:16:31 +0200

    Fix entity extraction in existing method

--- src/Plugin/Hal.php
+++ src/Plugin/Hal.php
@@ -1201,7 +1201,7 @@ class Hal extends AbstractHelper implements
      */
     protected function convertEntityToArray($entity)
     {
-        return $this->getResourceFactory()->convertEntityToArray($entity);
+        return $this->getEntityExtractor()->extract($entity);
     }
 
     /**
