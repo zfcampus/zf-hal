debd9334f673a2097b26c217704ff021e5566c30
webimpress <contact@webimpress.com>
2017-02-08 21:16:44 +0000

    Updated PHPUnit to ^5.7 || ^6.0
    - use `PHPUnit\Framework\TestCase` instead of
      `PHPUnit_Framework_TestCase`

--- test/Extractor/LinkCollectionExtractorTest.php
+++ test/Extractor/LinkCollectionExtractorTest.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @copyright Copyright (c) 2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2015-2017 Zend Technologies USA Inc. (http://www.zend.com)
  */
 
 namespace ZFTest\Hal\Extractor;
 
-use PHPUnit_Framework_TestCase as TestCase;
+use PHPUnit\Framework\TestCase;
 use ZF\Hal\Extractor\LinkCollectionExtractor;
 use ZF\Hal\Extractor\LinkExtractor;
 use ZF\Hal\Link\Link;
